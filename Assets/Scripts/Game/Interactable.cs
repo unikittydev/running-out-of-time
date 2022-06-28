e9f707da13de26d2b51d3c46814c0a75d1d9a855
unikittydev <777ruspacan777@gmail.com>
2022-06-28 21:30:19 +0300

    Minor Changes

---
+++ Assets/Scripts/Game/Interactable.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Game
+{
+    // Компонент для интерактивных объектов.
+    public class Interactable : MonoBehaviour
+    {
+        [SerializeField]
+        private Collider2D trigger;
+
+
+    }
+}
