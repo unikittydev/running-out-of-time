5fd16537188e4f8f5de95cfbc8807e225266aa85
unikittydev <777ruspacan777@gmail.com>
2022-06-29 13:01:46 +0300

    Minor Changes

--- Assets/Scripts/Player/CameraBlender.cs
+++ Assets/Scripts/Player/CameraBlender.cs
@@ -1,18 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
-public class CameraBlender : MonoBehaviour
+namespace Game
 {
-    // Start is called before the first frame update
-    void Start()
+    public class CameraBlender : MonoBehaviour
     {
-        
-    }
+        [System.Serializable]
+        public class BlenderSettings
+        {
+            public Transform anchorFrom;
+            public Transform anchorTo;
+            public CinemachineVirtualCamera camFrom;
+            public CinemachineVirtualCamera camTo;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [SerializeField]
+        private CinemachineBrain brain;
+
+        [SerializeField]
+        private CinemachineMixingCamera mixer;
+
+        [SerializeField]
+        private BlenderSettings[] blends;
+
+        [SerializeField]
+        private Transform target;
+
+        private void LateUpdate()
+        {
+            foreach (var blend in blends)
+            {
+                if (Between(target, blend.anchorFrom, blend.anchorTo))
+                {
+                }
+            }
+        }
+
+        private bool Between(Transform target, Transform from, Transform to)
+        {
+            return from.position.x > target.position.x && target.position.x < to.position.x;
+        }
     }
 }
