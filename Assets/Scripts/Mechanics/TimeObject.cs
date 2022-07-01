77ae1a5cd8b326de089d03cde7773570a2cd889a
Павел Кибирев <kpa.19@uni-dubna.ru>
2022-07-01 17:47:51 +0300

    Bubble finished, minor fixes

--- Assets/Scripts/Mechanics/TimeObject.cs
+++ Assets/Scripts/Mechanics/TimeObject.cs
@@ -9,8 +9,17 @@ public class TimeObject : MonoBehaviour
 	{
 		var renderer = GetComponent<SpriteRenderer>();
 		if (_time == TimeEpoch.Present)
+		{
 			renderer.maskInteraction = SpriteMaskInteraction.VisibleOutsideMask;
+			gameObject.layer = Utils.PRESENT_LAYER;
+		}
 		else
+		{
 			renderer.maskInteraction = SpriteMaskInteraction.VisibleInsideMask;
+			if (_time.HasFlag(TimeEpoch.Present))
+				gameObject.layer = 0;
+			else
+				gameObject.layer = Utils.PAST_LAYER;
+		}
 	}
 }
